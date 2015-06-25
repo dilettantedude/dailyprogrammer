dce7685aa1b7f92354c151fd65261008a593327b
dilettantedude <dilettantedude@outlook.com>
2015-06-24 22:54:22 -0500

    Daily Programmer E220

---
+++ DailyProgrammer/E220_Mangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Problem Statement: http://www.reddit.com/r/dailyprogrammer/comments/3aqvjn/20150622_challenge_220_easy_mangling_sentences/
+ * */
+namespace DailyProgrammer
+{
+    public class E220_Mangle
+    {
+        public static string MangeSentence(string input)
+        {
+            StringBuilder res = new StringBuilder(input.Length);
+            // Set string to lowercase
+            // Remove non characters
+            // Split the string
+            // Reverse individual components.
+            var reversedWords = new String(input.ToLower().Where(x => Char.IsLetter(x) || Char.IsWhiteSpace(x)).
+                                                ToArray()).
+                                                Split(new char[] { ' ' })
+                                                .Select(x => x.OrderBy(y=>y));
+
+            // Re-join the string keeping position in place.
+            int index = 0;
+            foreach(var word in reversedWords)
+            {
+                foreach (char c in word)
+                {
+                    if(!Char.IsLetter(input[index]))
+                    {
+                        while (!Char.IsLetter(input[index]))
+                        {
+                            res.Append(input[index++]);
+                        }
+                    }
+
+                    if (Char.IsUpper(input[index++]))
+                    {
+                        res.Append(c.ToString().ToUpper());
+                    }
+                    else
+                    {
+                        res.Append(c);
+                    }
+                }
+            }
+
+            while(index<input.Length)
+            {
+                res.Append(input[index++]);
+            }
+            return res.ToString();
+        }
+    }
+}
---
+++ DailyProgrammerTests/E220_Mange_Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DailyProgrammer;
+
+namespace DailyProgrammerTests
+{
+    [TestClass]
+    public class E220_Mange_Tests
+    {
+        [TestMethod]
+        public void Mangle_Not_So_Hard()
+        {
+            string input = "This challenge doesn't seem so hard.";
+            string output = "Hist aceeghlln denos't eems os adhr.";
+
+            Assert.AreEqual(output, E220_Mangle.MangeSentence(input), "Failed to match mangled text");
+        }
+
+        [TestMethod]
+        public void Heaven_And_Earth()
+        {
+            string input = "There are more things between heaven and earth, Horatio, than are dreamt of in your philosophy.";
+            string output = "Eehrt aer emor ghinst beeentw aeehnv adn aehrt, Ahioort, ahnt aer ademrt fo in oruy hhilooppsy.";
+
+            Assert.AreEqual(output, E220_Mangle.MangeSentence(input), "Failed to match mangled text");
+        }
+        [TestMethod]
+        public void Eye_of_a_Newt()
+        {
+            string input = "Eye of Newt, and Toe of Frog, Wool of Bat, and Tongue of Dog.";
+            string output = "Eey fo Entw, adn Eot fo Fgor, Loow fo Abt, adn Egnotu fo Dgo.";
+
+            Assert.AreEqual(output, E220_Mangle.MangeSentence(input), "Failed to match mangled text");
+        }
+        [TestMethod]
+        public void Adders_Fork()
+        {
+            string input = "Adder's fork, and Blind-worm's sting, Lizard's leg, and Howlet's wing.";
+            string output = "Adder's fkor, adn Bdilm-nors'w ginst, Adilrs'z egl, adn Ehlost'w ginw.";
+
+            Assert.AreEqual(output, E220_Mangle.MangeSentence(input), "Failed to match mangled text");
+        }
+        [TestMethod]
+        public void For_a_charm()
+        {
+            string input = "For a charm of powerful trouble, like a hell-broth boil and bubble.";
+            string output = "For a achmr fo eflopruw belortu, eikl a behh-llort bilo adn bbbelu.";
+
+            Assert.AreEqual(output, E220_Mangle.MangeSentence(input), "Failed to match mangled text");
+        }
+    }
+}
